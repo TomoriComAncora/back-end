0c763b7bb195b9595825e1e782438a453494936b
TomoriComAncora <lucas_fernando@ufms.br>
2023-09-20 16:03:50 -0400

    pratica_com_args

---
+++ 2_FUNDAMENTOS/6_pratica_com_args/index.js
@@ -0,0 +1,11 @@
+// modulo_externo
+const minimist = require("minimist");
+
+// modulo_interno
+const soma = require("./soma").soma;
+
+const args = minimist(process.argv.slice(2));
+
+const a = parseInt(args["a"]);
+const b = parseInt(args["b"]);
+soma(a, b);
