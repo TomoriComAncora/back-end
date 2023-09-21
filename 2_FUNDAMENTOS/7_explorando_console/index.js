ce92c90fddc2a34cebf39dea79bce3e1b859c737
TomoriComAncora <lucas_fernando@ufms.br>
2023-09-21 10:44:10 -0400

    console

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,16 @@
+const x = 10;
+const y = [1, 3, 5];
+const z = "Lucas";
+console.log(x, y, z);
+
+// count
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de y é: ${y}, contagem`);
+
+// clear
+setTimeout(() => {
+  console.clear();
+}, 2000);
